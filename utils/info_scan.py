8582f51e4b6fd9fbb42798e0357f6a5c017078e8
daguy666 <jpistonejr@gmail.com>
2018-06-13 11:26:02 -0400

    Actually saved the file this time, so the change can be put in git.

--- utils/info_scan.py
+++ utils/info_scan.py
@@ -53,58 +53,6 @@ class Informational_Scan(object):
         for user in self.gsi.return_list_of_users():
             self.gsi.return_cron_tab(user)
        
-       #================================================   
-       #TODO finish the cron output
-       #TODO check on the return open with internet
-       #================================================
-
-    def ___build_up_system_info(self):
-        """
-        Starts building up some system info
-        """
-        # Print put system time
-        system_time = datetime.today().strftime('%Y-%m-%d--%H:%M:%S')
-        print self.lines
-        print "[+] System Time: %s" % system_time
-        # Print some info about the processor
-        print "[+] Processor info: %s " % self.gsi.return_platform_info()
-        # Print out the computer hostname
-        print "[+] Hostname: %s" % self.gsi.return_hostname()
-        # Print out home directories
-        print "[+] Home Directories Present: "
-        for i in self.gsi.return_home_dirs():
-            print "\t[-]%s" % i
-        print self.lines
-        # Print OS Version
-        print "[+] OS Version: %s " % self.gsi.return_os_version()
-        # Print OS Build
-        print "[+] OS Build: %s " % self.gsi.return_os_build()
-        # Print OS Name
-        print "[+] OS Name: %s" % self.gsi.return_os_version_name().capitalize()
-        # Print shutdown and reboot status. 
-        print self.lines
-        print "[+] Shutdown history: "
-        for i in self.gsi.show_last_shutdown():
-            print "\t[-] %s" % i
-        print self.lines
-        print "[+] Reboot history: "
-        for i in self.gsi.show_last_reboot():
-            print "\t[-] %s " % i
-        print self.lines
-        # Print out wireless networks
-        for wifi in self.gsi.return_wireless_networks():
-            print "[+] %s" % wifi.strip()
-        print self.lines
-        for open_files_i in self.gsi.return_open_with_internet():
-            print "[+] %s" % open_files_i
-        print self.lines
-        for open_files in self.gsi.return_open_files():
-            print "[+] %s" % open_files
-
-        print self.lines
-        for i in self.gsi.return_list_of_users():
-            if len(self.gsi.return_crons(i)) > 1:
-                print self.gsi.return_crons(i)
 
 
     def main(self):
